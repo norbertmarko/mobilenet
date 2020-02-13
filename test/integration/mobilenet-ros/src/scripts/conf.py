f84af1b73793327317230fba32b0a8ac7ff90421
norbertmarko <norbertmarko92@gmail.com>
2020-02-13 13:44:37 +0100

    mobilenet-ros config changes

--- test/integration/mobilenet-ros/src/scripts/conf.py
+++ test/integration/mobilenet-ros/src/scripts/conf.py
@@ -1,19 +1,20 @@
 import os
 
-os.chdir('../..')
-trt_opt_model = 'optimizer/export/trt_savedmodel/freezed_model_trt.pb'
-
-# os.chdir changes path, give image path relative to that
-single_image_path = './test/benchmarks/um_000000.png'
-image_batch_path = './test/benchmarks/test_set'
+trt_opt_model = '/media/orion/6400F60300F5DC4C/nn_experiment/camera_test/mobilenet-master/optimization/export/trt_savedmodel/freezed_model_trt.pb'
 
 height = 288 
 width = 512
 
+# TF config
+
 inputTensor = 'input_1:0'
 softmaxTensor = 'conv2d_transpose_2/truediv:0'
 
-# color maps for number of classes and mask coloring
+# ROS config
+
+topic = 'webcam/image_raw'
+
+# color maps for mask coloring
 
 CLASSES = {(1,64,128) : 0,
            (3,143,255) : 1, 
@@ -33,6 +34,3 @@ colors_bgr = [[1, 64,128],
               [255,140,0],
               [0,  0,  0]]
 
-# ROS config
-
-topic = 'webcam/image_raw'
