c3747a8200efa9550ea8cfe6fee93827ac53ab4b
norbertmarko <norbertmarko92@gmail.com>
2020-02-19 14:04:11 +0100

    Basic transformation pipeline.

--- test/integration/mobilenet-ros/src/scripts/tr_pred_node.py
+++ test/integration/mobilenet-ros/src/scripts/tr_pred_node.py
@@ -8,19 +8,46 @@ import numpy as np
 
 import rospy
 from sensor_msgs.msg import Image
+from visualization_msgs.msg import MarkerArray
 from cv_bridge import CvBridge, CvBridgeError
 
-class TRPRED():
+from conf import height as h
+from conf import width as w
+
+class TransformPrediction():
+
     def __init__(self):
         self._cv_bridge = CvBridge()
         self._sub = rospy.Subscriber('tfros_seg', Image,
                                         self.callback, queue_size=1)
+
+    def _birds_eye_transform(self, frame, source, destination):
+        # Transformation Matrix
+        tr_matrix = cv2.getPerspectiveTransform(source, destination)
+        return cv2.warpPerspective(frame, tr_matrix, (w, h))
     
     def callback(self, msg):
-        decodedFrame = self._cv_bridge.imgmsg_to_cv2(msg, 'brg8')
-        
+        decodedFrame = self._cv_bridge.imgmsg_to_cv2(msg, 'bgr8')
 
-        mrk_arr_msg = "" # should publish a marker array rosmsg
+        # Source Points - point pairs - (top left, top right, bottom left, bottom right)
+        sourcePoints = np.float32([[0, 0], [0, 0], [0, 0], [0, 0]])
+
+        # Destination Points
+        destinationPoints = np.float32([[0, 0], [0, 0], [0, 0], [0, 0]])
+
+        warpedFrame = self._birds_eye_transform(decodedFrame,
+                                                sourcePoints, destinationPoints)
+
+        # extract color information - road
+        road_arr = warpedFrame[0:140:255]
+
+        # Extract Markers
+
+        mrk_arr_msg = "" # should publish a MarkerArray rosmsg
+        
+        # Publish MarkerArray
+        pub = rospy.Publisher('markerarray_seg', MarkerArray, queue_size=1)
+        pub.publish(mrk_arr_msg)
 
     def main(self):
         rospy.spin()
@@ -28,5 +55,5 @@ class TRPRED():
 
 if __name__ == '__main__':
     rospy.init_node('tr_pred')
-    trpred = TRPRED()
+    trpred = TransformPrediction()
     trpred.main()
